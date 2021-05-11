18f8670f65a4eea9834a351f20797485df989a51
JaeiK <83495421+JoJae1k@users.noreply.github.com>
2021-05-11 19:56:33 +0900

    Create hs0430 - 2.py

---
+++ python3rd.py/hs0430 - 2.py
@@ -0,0 +1,8 @@
+print("x, y, z 3개의 값을 입력하시오: ")
+x = int(input("x의 값을 입력: "))
+y = int(input("y의 값을 입력: ")) 
+z = int(input("z의 값을 입력: ")) 
+x = x + 1
+y = y + 1
+z = z + 1
+print("변경된 값은:", x, y, z)
