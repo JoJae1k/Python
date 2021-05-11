7cb937d1a2f3cf7126fa327f838d88abc1b4973d
JaeiK <83495421+JoJae1k@users.noreply.github.com>
2021-05-11 19:56:29 +0900

    Create hs0430 - 1.py

---
+++ python3rd.py/hs0430 - 1.py
@@ -0,0 +1,9 @@
+total = 0
+counter = 1
+while counter <= 10:
+    grade = int(input ("grade를 입력하세요.: ")) 
+    total = total + grade
+    counter = counter + 1
+    print(counter)
+average = total / 10
+print(average)
