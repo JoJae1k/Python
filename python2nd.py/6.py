cc1631522871714c0cebe0c2678a4725ec0ea197
JaeiK <83495421+JoJae1k@users.noreply.github.com>
2021-05-12 17:29:21 +0900

    Update 6.py

--- python2nd.py/6.py
+++ python2nd.py/6.py
@@ -1,17 +1,17 @@
-def chat(name1, name2):
-    print("%s: 안녕? 너 몇살이니?" %name1)
-    print("%s: 나? 나는 20살이야!" %name2)
-chat("정수", "태희")
-chat("은지", "가연")
+def chat(name1, name2): #함수명을 chat으로 매개변수를 name1, name2로 선언
+    print("%s: 안녕? 너 몇살이니?" %name1) #문자형 "name1 : 안녕? 너 몇살이니?" 출력"
+    print("%s: 나? 나는 20살이야!" %name2) #문자형 "name2 : 나? 나는 20살이야!" 출력"
+chat("정수", "태희") #name1을 정수, name2를 태희로 선언
+chat("은지", "가연") #name1을 정수, name2를 태희로 선언
 
-a = 3
-b = 2
-c = 5
-d = c-a
+a = 3 #변수 a (숫자형 3 으로 선언)
+b = 2 #변수 b (숫자형 2 으로 선언)
+c = 5 #변수 c (숫자형 5 으로 선언)
+d = c-a #변수 d (문자형 c를 a 로 뺀 값으로 선언)
 
-print(a + b)
-print(a * d)
-print(b % d)
+print(a + b) #문자형 a와 b를 더한 값 출력
+print(a * d) #문자형 a와 d를 곲한 값 출력
+print(b % d) #문자형 b와 d를 나눈 값 출력
 
 if a > b :
     print("Hello 0_0")
