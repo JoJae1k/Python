33b79d530cf959218d9342924032372983cd55fd
JaeiK <83495421+JoJae1k@users.noreply.github.com>
2021-05-12 17:20:19 +0900

    Update 5.py

--- python2nd.py/5.py
+++ python2nd.py/5.py
@@ -1,11 +1,19 @@
-x = True #대문자로 쓰기
-y = False
+x = True #변수 x (문자형 "True" 으로 선언)
+y = False #변수 y (문자형 "False" 으로 선언)
 
-print(x,y)
-print(x)
-print(y)
+#반드시 대문자로 쓰기
 
-a = 5
-b = 4
+print(x,y) #문자형 x, y 출력
+print(x) #문자형 x 출력
+print(y) #문자형 y 출력
+
+a = 5 #변수 a (숫자형 5 으로 선언)
+b = 4 #변수 b (숫자형 4 으로 선언)
 if a > b :
     print("출력")
+else :
+    print(y)
+    
+#만약 a가 b보다 크면 문자형 "출력" 을 출력하고 그게 아니면 문자형 y를 출력해라
+
+#결론 : if 구문은 유용하니까 외우자.
